827cf8350fbaef4d81c04d83bd63893c0b3e60ee
Navaneeth Nivol <33155848+navaneethnivol@users.noreply.github.com>
2024-03-19 09:43:56 -0700

    added google login

--- dev/frontend/components/navbar.tsx
+++ dev/frontend/components/navbar.tsx
@@ -1,27 +1,43 @@
 "use client"
 
 import Image from "next/image";
-import { useGoogleOneTapLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 
 import Logo from "@/static/logo.png";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+
+interface Login {
+    aud: string
+    azp: string
+    email: string
+    email_verified: boolean
+    exp: number
+    family_name: string
+    given_name: string
+    hd: string
+    iat: string
+    iss: string
+    jti: string
+    locale: string
+    name: string
+    nbf: number
+    picture: string
+    sub: string
+}
 
 export default function NavBar() {
 
-    useGoogleOneTapLogin({
-        onSuccess: credentialResponse => {
-            console.log(credentialResponse);
-            if (credentialResponse.credential) {
-                const decodedToken = jwtDecode(credentialResponse.credential);
-                console.log(decodedToken);
-            }
-        },
-        onError: () => {
-            console.log('Login Failed');
-            // Handle error
-        },
-    });
+    const [login, setLogin] = useState<Login>();
+
+    useEffect(() => {
+        let login = localStorage.getItem("login");
+        if (login) {
+            setLogin(JSON.parse(login));
+        }
+    }, []);
 
     return (
         <header>
@@ -29,6 +45,32 @@ export default function NavBar() {
                 <Link href="/">
                     <Image src={Logo} alt="NumRecall Logo" height={40} ></Image>
                 </Link>
+                {
+                    login && Object.keys(login).length > 0
+                        ? <div className="flex flex-row">
+                            <div className="flex flex-row items-center bg-[#D3D3D3] p-1.5 rounded-full mr-3">
+                                <img src={login.picture} alt="User image" className="w-8 h-8 rounded-full mr-3" />
+                                <p>{login.name}</p>
+                            </div>
+                            <button onClick={() => {
+                                setLogin(undefined);
+                                localStorage.removeItem("login");
+                            }}>X</button>
+                        </div>
+                        : <GoogleLogin onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                            if (credentialResponse.credential) {
+                                const decodedToken: Login = jwtDecode(credentialResponse.credential);
+                                console.log(decodedToken);
+                                localStorage.setItem("login", JSON.stringify(decodedToken));
+                                setLogin(decodedToken);
+                            }
+                        }}
+                            onError={() => {
+                                console.log('Login Failed');
+                            }}
+                        />
+                }
             </div>
         </header>
     );
